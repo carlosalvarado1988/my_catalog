5c97139696c1e8a81d6deaf415c0be986c1d3b86
Carlos Alvarado <carlos@generalui.com>
2020-06-07 18:27:39 -0600

    reestructure webapp and server projects in one dev enviroment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 8080;
 
